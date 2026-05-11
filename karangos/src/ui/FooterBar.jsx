38df96983b3836227ab3b212b77e42a0d7157c29
SilvioPeroni <silviocezar08@gmail.com>
2026-05-11 14:27:40 +0000

    (11/05) Implementação do menu principal e da barra de rodapé

---
+++ karangos/src/ui/FooterBar.jsx
@@ -0,0 +1,34 @@
+
+import React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CoffeeIcon from '@mui/icons-material/Coffee';
+
+export default function FooterBar() {
+return <>
+  <Box
+    component="footer"
+    sx={{
+      backgroundColor: 'action.disabledBackground',
+      display: 'flex',
+      justifyContent: 'center',
+      position: 'fixed',  // posição fixa
+      bottom: 0,          // na parte de baixo da página
+      width: '100vw'
+    }}
+    >
+    <Typography
+        variant="caption"
+        sx={{
+         '& a': {  // Altera a cor do link (a) dentro do Typography (&)
+           color: 'secondary.main'
+         }
+       }}
+        >
+      Desenvolvido e mantido com <CoffeeIcon  sx={{ verticalAlign: 'text-bottom', fontSize: 
+        'inherit' }} /> por <a href="mailto:silviocezar@gmail.com">Silvio Peroni</a>
+    </Typography>
+    </Box>
+  </>
+}
+
