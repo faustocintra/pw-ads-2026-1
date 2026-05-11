b01bb0e652b3816636ec199ae76ba8cf21087bdd
Assuel J. Andrade <161782571+assueljandrade@users.noreply.github.com>
2026-05-11 14:05:01 +0000

    (11/05) Implementação do menu principal e da barra de rodapé

---
+++ karangos/src/App.jsx
@@ -0,0 +1,23 @@
+// import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import HeaderBar from './ui/HeaderBar'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './ui/theme'
+import FooterBar from './ui/FooterBar'
+
+function App() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <HeaderBar />
+          <FooterBar />
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
