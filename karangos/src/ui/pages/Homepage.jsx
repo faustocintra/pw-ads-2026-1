d3a94d6fbd9e8124b79f6042c53cd5c91b3003ef
Rafaela Lissa <paraaplicativos259@gmail.com>
2026-05-18 14:41:16 +0000

    (18/05) Páginas, rotas e primeiro Data Grid

---
+++ karangos/src/ui/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+
+export default function Homepage() {
+ return <>
+   <Typography variant="h1">
+     Bem-vindo(a) à loja Karangos!
+   </Typography>
+ </>
+}
+
